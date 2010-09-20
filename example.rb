afd06313d4fb3aa140a5ade740a4634eef38523c
Phil Pirj <pirj@mail.ru>
2010-09-21 02:25:03 +0400

    size and times work fine

--- example.rb
+++ example.rb
@@ -14,20 +14,24 @@ File.open('/remote/111/file1.txt', 'wb') do |file|
   file.write('writing to a new remotely stored file')
 end
 
-File.open('/remote/111/file1.txt', 'wb') do |file|
+File.open('/remote/111/file1.txt') do |file|
   p "readall:[#{file.read}]"
 end
 
-File.open('/remote/111/file1.txt', 'wb') do |file|
+File.open('/remote/111/file1.txt') do |file|
   x = file.read 10
   p "read 10:[#{x}]"
 end
 
-File.open('/remote/111/file1.txt', 'wb') do |file|
+File.open('/remote/111/file1.txt') do |file|
   x = file.read 10, 3
   p "read 10,3:[#{x}]"
 end
 
-p File.size('/remote/111/file1.txt')
+p "size=#{File.size('/remote/111/file1.txt')}"
+stat = File.stat('/remote/111/file1.txt')
+p "atime: #{stat.atime}"
+p "ctime: #{stat.ctime}"
+p "mtime: #{stat.mtime}"
 
 # puts Dir.entries('/remote')
--- lib/dshelf/stat.rb
+++ lib/dshelf/stat.rb
@@ -1,14 +1,15 @@
 require 'dshelf'
 require 'json'
+require 'time'
 
 class DistributedStat
   include DistributedShelf
   def path
-    @path
+    @dpath
   end
 
   def initialize filename
-    @path = filename
+    @dpath = filename
     RestClient.get("#{server_url}/stat#{absolutepath}", {:accept => :json}) do |response, request, result|
       case response.code
       when 200
@@ -20,15 +21,38 @@ class DistributedStat
   end
   
   def parse_stat stat
-    @stat = JSON.parse stat
-    p "stat=#{@stat}"
+    parsed = JSON.parse stat
+    @dstat = {}
+    @dstat[:size] = parsed['size']
+    @dstat[:atime] = Time.parse parsed['atime']
+    @dstat[:ctime] = Time.parse parsed['ctime']
+    @dstat[:mtime] = Time.parse parsed['mtime']
+  end
+
+  def setgid?; true end
+  def setuid?; true end
+
+  def size
+    @dstat[:size]
+  end
+  
+  def atime
+    @dstat[:atime]
+  end
+
+  def ctime
+    @dstat[:ctime]
+  end
+
+  def mtime
+    @dstat[:mtime]
   end
 
     # atime   blksize   blockdev?   blocks   chardev?   ctime   dev   
     # dev_major   dev_minor   directory?   executable?   executable_real?
     # file?   ftype   gid   grpowned?   ino   inspect   mode   mtime   new
     # nlink   owned?   pipe?   pretty_print   rdev   rdev_major   rdev_minor
-    # readable?   readable_real?   setgid?   setuid?   size   size?   socket?
+    # readable?   readable_real?      size   size?   socket?
     # sticky?   symlink?   uid   writable?   writable_real?   zero? 
 
   # def method_missing method
